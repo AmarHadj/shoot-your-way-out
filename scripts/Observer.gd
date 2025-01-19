30ff785dd30fedadbcdb90420c7b01690cea2cde
AmarHadj <Planteramartin@gmail.com>
2025-01-18 19:04:05 -0500

    Initial commit

---
+++ scripts/Observer.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var shootingPhase = false
+
+#set the phase to shooting phase
+func setPhaseToShoot():
+	if !shootingPhase :
+		shootingPhase = true
---
+++ scripts/shoot_button.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+# This code will switch into shoot mode and the player will se the results of his guns placement
+func _on_button_pressed() -> void:
+	Observer.setPhaseToShoot()
