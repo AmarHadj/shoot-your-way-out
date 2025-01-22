c8cd1d98ef4cc8fcd866ac7169239cf4625fe32f
AmarHadj <Planteramartin@gmail.com>
2025-01-21 20:57:18 -0500

    weapon slot system finished

--- scripts/drop_zone.gd
+++ scripts/drop_zone.gd
@@ -1,5 +1,10 @@
 extends Marker2D
-
+var has_weapon = false
 
 func _draw():
 	draw_circle(Vector2.ZERO, 50, Color.DARK_BLUE)
+func set_has_weapon(placed : bool):
+	has_weapon = placed
+
+func get_has_weapon():
+	return has_weapon
--- scripts/revolver.gd
+++ scripts/revolver.gd
@@ -4,7 +4,7 @@ const min_dist = 50
 var selected = false
 var rest_point
 var is_placed
-var new_position
+var new_position = -1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -22,13 +22,19 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 func _physics_process(delta):
 	if selected:
 		global_position=lerp(global_position, get_global_mouse_position(), 25*delta)
+		is_placed = false		
+		if new_position >= 0:
+			if Observer.weapon_slots[new_position].get_has_weapon():
+				Observer.weapon_slots[new_position].set_has_weapon(false)
+				print(Observer.weapon_slots[new_position].get_has_weapon())
 	else :
-		place_gun()
+		if !is_placed :
+			place_gun()
 		if is_placed :
-			global_position = lerp(global_position, new_position.global_position, 10* delta)
-			is_placed = false
+			global_position = lerp(global_position, Observer.weapon_slots[new_position].global_position, 10* delta)
 		else :
 			global_position = lerp(global_position, rest_point.global_position, 10* delta)
+			new_position = -1
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -39,10 +45,11 @@ func set_rest_point(rest_point_choosed : Marker2D):
 	rest_point = rest_point_choosed
 	
 func place_gun():
-	for child in Observer.weapon_slots:
-		if abs(global_position.x - child.global_position.x) < min_dist and abs(global_position.y - child.global_position.y) < min_dist:
+	for i in range(Observer.weapon_slots.size()):
+		if abs(global_position.x - Observer.weapon_slots[i].global_position.x) < min_dist and abs(global_position.y - Observer.weapon_slots[i].global_position.y) < min_dist and !Observer.weapon_slots[i].get_has_weapon():
 			is_placed = true
-			new_position = child
+			Observer.weapon_slots[i].set_has_weapon(true)
+			new_position = i
 			break
 		
 	
