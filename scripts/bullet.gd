34f6d5390c5fb888206a9767151ea3a296b7c419
AmarHadj <Planteramartin@gmail.com>
2025-01-23 10:23:54 -0500

    code cleaning

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,24 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 300.0 # set to 2000.0 for the final result
 var x_movment = SPEED
 var y_movment = 0
 
-
-
 func _physics_process(delta):
-
 	if Observer.shootingPhase:
 		velocity.x = x_movment
 		velocity.y = y_movment
 		move_and_slide()
 
-
 func _on_area_2d_area_entered(area: Area2D):
 	if area.name == "shoot_area" :
+		
+		# sets the position of the bullet on the gun barrel
+		for child in area.get_children():
+			if child.name == "barrel":
+				self.global_position = child.global_position
+		
 		var gun_rotation = area.get_parent().get_parent().get_rotation_applied()
 		self.set_rotation_degrees(gun_rotation)
+
 		if gun_rotation == 0 :
 			x_movment = SPEED
 			y_movment = 0
--- scripts/revolver.gd
+++ scripts/revolver.gd
@@ -1,25 +1,16 @@
 extends Node2D
 
-const min_dist = 50
+const min_dist = 50 # the minimum distance for a gun to be at a weapon slot to get attached at it
+var new_position = -1 #represent the position on the weapon slots array where the gun will be placed
 var selected = false
-var rest_point
+var inventory_slot # the inventory slot that the gun will have
 var is_placed
-var new_position = -1
-var is_rotating
+
 var rotation_angle = 90
 var rotation_applied = 0
 
-
-# Called when the node enters the scene tree for the first time.
-
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	
+# manage the mouse click input
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if !Observer.shootingPhase:	
 		if Input.is_action_just_pressed("click"):
 			selected = true
@@ -34,6 +25,7 @@ func _physics_process(delta):
 		if selected:
 			global_position=lerp(global_position, get_global_mouse_position(), 25*delta)
 			is_placed = false		
+			# if new_position < 0 then no weapon slot has been assigned
 			if new_position >= 0:
 				if Observer.weapon_slots[new_position].get_has_weapon():
 					Observer.weapon_slots[new_position].set_has_weapon(false)
@@ -43,7 +35,7 @@ func _physics_process(delta):
 			if is_placed :
 				global_position = lerp(global_position, Observer.weapon_slots[new_position].global_position, 10* delta)
 			else :
-				global_position = lerp(global_position, rest_point.global_position, 10* delta)
+				global_position = lerp(global_position, inventory_slot.global_position, 10* delta)
 				new_position = -1
 
 func _input(event):
@@ -51,12 +43,8 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
 			
-		if event.button_index == MOUSE_BUTTON_RIGHT and not event.pressed:
-			is_rotating = false
-			
-func set_rest_point(rest_point_choosed : Marker2D):
-	rest_point = rest_point_choosed
-	
+# this function will compare the global position of the gun with the global position of the weapon
+# slot and decide if the difference is close enough to attach the gun to the weapon slot
 func place_gun():
 	for i in range(Observer.weapon_slots.size()):
 		if abs(global_position.x - Observer.weapon_slots[i].global_position.x) < min_dist and abs(global_position.y - Observer.weapon_slots[i].global_position.y) < min_dist and !Observer.weapon_slots[i].get_has_weapon():
@@ -67,5 +55,8 @@ func place_gun():
 
 func get_rotation_applied():
 	return rotation_applied
+	
+func set_rest_point(inventory_slot_choosed : Marker2D):
+	inventory_slot = inventory_slot_choosed
 		
 	
